e71839851fa6cf50165287c412f03302b1bbfea1
jakeadelman <jacobzadelman@gmail.com>
2018-11-26 22:35:01 -0500

    fixed schema

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -2,105 +2,105 @@
 // graphql typescript definitions
 
 export namespace GQL {
-interface IGraphQLResponseRoot {
-data?: IQuery | IMutation;
-errors?: Array<IGraphQLResponseError>;
-}
+  interface IGraphQLResponseRoot {
+    data?: IQuery | IMutation;
+    errors?: Array<IGraphQLResponseError>;
+  }
 
-interface IGraphQLResponseError {
-/** Required for all errors */
-message: string;
-locations?: Array<IGraphQLResponseErrorLocation>;
-/** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
-[propName: string]: any;
-}
+  interface IGraphQLResponseError {
+    /** Required for all errors */
+    message: string;
+    locations?: Array<IGraphQLResponseErrorLocation>;
+    /** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
+    [propName: string]: any;
+  }
 
-interface IGraphQLResponseErrorLocation {
-line: number;
-column: number;
-}
+  interface IGraphQLResponseErrorLocation {
+    line: number;
+    column: number;
+  }
 
-interface IQuery {
-__typename: "Query";
-hello: string;
-quote: Array<IQuote | null> | null;
-tick: Array<ITick | null> | null;
-}
+  interface IQuery {
+    __typename: "Query";
+    hello: string;
+    quote: Array<IQuote | null> | null;
+    tick: Array<ITick | null> | null;
+  }
 
-interface IHelloOnQueryArguments {
-name?: string | null;
-}
+  interface IHelloOnQueryArguments {
+    name?: string | null;
+  }
 
-interface IQuoteOnQueryArguments {
-timestamp?: string | null;
-symbol?: string | null;
-bidSize?: number | null;
-bidPrice?: number | null;
-askPrice?: number | null;
-askSize?: number | null;
-}
+  interface IQuoteOnQueryArguments {
+    timestamp?: string | null;
+    symbol?: string | null;
+    bidSize?: number | null;
+    bidPrice?: number | null;
+    askPrice?: number | null;
+    askSize?: number | null;
+  }
 
-interface ITickOnQueryArguments {
-timestamp?: string | null;
-symbol?: string | null;
-side?: string | null;
-size?: number | null;
-price?: number | null;
-tickDirection?: string | null;
-trdMatchID?: string | null;
-}
+  interface ITickOnQueryArguments {
+    timestamp?: string | null;
+    symbol?: string | null;
+    side?: string | null;
+    size?: number | null;
+    price?: number | null;
+    tickDirection?: string | null;
+    trdMatchID?: string | null;
+  }
 
-interface IQuote {
-__typename: "Quote";
-timestamp: string;
-symbol: string;
-bidSize: number;
-bidPrice: number;
-askPrice: number;
-askSize: number;
-}
+  interface IQuote {
+    __typename: "Quote";
+    timestamp: string;
+    symbol: string;
+    bidSize: number;
+    bidPrice: number;
+    askPrice: number;
+    askSize: number;
+  }
 
-interface ITick {
-__typename: "Tick";
-timestamp: string;
-symbol: string;
-side: string;
-size: number;
-price: number;
-tickDirection: string;
-trdMatchID: string;
-}
+  interface ITick {
+    __typename: "Tick";
+    timestamp: string;
+    symbol: string;
+    side: string;
+    size: number;
+    price: number;
+    tickDirection: string;
+    trdMatchID: string;
+  }
 
-interface IMutation {
-__typename: "Mutation";
-register: boolean | null;
-newquote: boolean | null;
-newtick: boolean | null;
-}
+  interface IMutation {
+    __typename: "Mutation";
+    register: boolean | null;
+    newquote: boolean | null;
+    newtick: boolean | null;
+  }
 
-interface IRegisterOnMutationArguments {
-email: string;
-password: string;
-}
+  interface IRegisterOnMutationArguments {
+    email: string;
+    password: string;
+  }
 
-interface INewquoteOnMutationArguments {
-timestamp: string;
-symbol: string;
-bidSize: number;
-bidPrice: number;
-askPrice: number;
-askSize: number;
-}
+  interface INewquoteOnMutationArguments {
+    timestamp: string;
+    symbol: string;
+    bidSize: number;
+    bidPrice: number;
+    askPrice: number;
+    askSize: number;
+  }
 
-interface INewtickOnMutationArguments {
-timestamp: string;
-symbol: string;
-side: string;
-size: number;
-price: number;
-tickDirection: string;
-trdMatchID: string;
-}
+  interface INewtickOnMutationArguments {
+    timestamp: string;
+    symbol: string;
+    side: string;
+    size: number;
+    price: number;
+    tickDirection: string;
+    trdMatchID: string;
+  }
 }
 
 // tslint:enable
