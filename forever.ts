2c2202e4010503f0b6756f6f887f3a4e9d2c56d5
jake manx <jacobzadelman@gmail.com>
2018-12-23 00:14:36 -0500

    forever config changes

--- forever.ts
+++ forever.ts
@@ -1,16 +1,16 @@
 var forever = require('forever-monitor')
 
 var child = new forever.Monitor('src/index.ts', {
-  max: 10,
+  max: 15,
   silent: true,
   args: [],
   command: 'ts-node',
   minUptime: 2000,
-  spinSleepTime: 10000
+  spinSleepTime: 100000
 })
 
 child.on('exit', function() {
-  console.log('process exited after 3 restarts')
+  console.log('process exited after 15 restarts')
 })
 
 child.start()
