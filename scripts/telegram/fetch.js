e1f0fc5eb1f3b557fe701dafab8a5fecb866a523
jake manx <jacobzadelman@gmail.com>
2019-01-13 14:13:11 -0500

    commit

---
+++ scripts/telegram/fetch.js
@@ -0,0 +1,16 @@
+import MTProto from 'telegram-mtproto'
+
+const phone = {
+    num: '+16479668071',
+    code: '37300'
+}
+
+const api = {
+    layer: ,
+    api_id: 628342
+}
+
+key: 628342 
+
+hash: e6bc86d889cbfaf5144a15e0f3966a7f 
+servers: 149.154.167.40:443
